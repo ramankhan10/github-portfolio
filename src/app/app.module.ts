eddcf469d8b1c558b94409bb2f2ad1f9efc9a6b7
Raman <khanahmadi.raman@gmail.com>
2023-10-08 03:14:47 -0700

    Listing user repositories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { PanelComponent } from './panel/panel.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonalInfoComponent],
+  declarations: [AppComponent, PersonalInfoComponent, PanelComponent, RepositoriesComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from './user';
+import { Repository } from './repository';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,15 @@ import { User } from './user';
 export class GithubService {
 
   username = 'ramankhan10';
-  private apiUrl = 'https://api.github.com';
+  private apiUrl = 'https://api.github.com/users/' + this.username;
 
   constructor(private http: HttpClient) { }
 
   getUser(): Observable<User> {
-    return this.http.get<User>(`${this.apiUrl}/users/${this.username}`)
+    return this.http.get<User>(this.apiUrl)
+  }
+
+  getRepos(): Observable<Repository[]> {
+    return this.http.get<Repository[]>(this.apiUrl + '/repos')
   }
 }
