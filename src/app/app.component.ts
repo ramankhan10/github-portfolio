5718cae7729c9ff243a3faa70434cf2869a87fd1
Raman <khanahmadi.raman@gmail.com>
2023-10-08 11:56:13 -0700

    Enhancing SEO capabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from './github.service';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,24 @@ export class AppComponent implements OnInit {
 
   username = '';
 
-  constructor(private gitHubService: GithubService) { }
+  constructor(
+    private gitHubService: GithubService,
+    private titleService: Title,
+    private meta: Meta
+  ) {}
 
   ngOnInit(): void {
     this.username = this.gitHubService.username;
+    this.titleService.setTitle('GitHub portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s GitHub portfolio'`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
   }
 }
