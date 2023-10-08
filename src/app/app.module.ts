c62ea39ef4041d215ec6a5b4c06dc6453fb2b1f0
Raman <khanahmadi.raman@gmail.com>
2023-10-08 11:40:23 -0700

    Prerendering content during build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
@@ -10,8 +11,8 @@ import { OrganizationsComponent } from './organizations/organizations.component'
 
 @NgModule({
   declarations: [AppComponent, PersonalInfoComponent, PanelComponent, RepositoriesComponent, OrganizationsComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, TransferHttpCacheModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
